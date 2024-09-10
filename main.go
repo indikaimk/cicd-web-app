af445b72507f15b46bb99a0eec8e5416c5c231b7
cloudqubes <cloudqubes@gmail.com>
2024-09-10 11:13:21 +0530

    update

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func hello(w http.ResponseWriter, req *http.Request) {
 }
 
 func version(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "v1.0.200")
+	io.WriteString(w, "v1.0.14")
 }
 
 func main() {
